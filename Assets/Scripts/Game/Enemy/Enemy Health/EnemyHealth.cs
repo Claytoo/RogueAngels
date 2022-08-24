56e6230421e042168dd75819a9ff6870a3d6ef9c
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2022-08-24 20:39:41 +0700

    add: enemy health with slider UI

---
+++ Assets/Scripts/Game/Enemy/Enemy Health/EnemyHealth.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("Health Value")]
+    public float totalHealth;
+    public float currentHealth;
+
+    [Header("UI Slider")] 
+    public Vector3 offset;
+    public Slider healthSlider;
+
+    private void Start()
+    {
+        currentHealth = totalHealth;
+        healthSlider.maxValue = totalHealth;
+        healthSlider.value = totalHealth;
+    }
+
+    private void Update()
+    {
+        healthSlider.transform.position = Camera.main.WorldToScreenPoint(transform.position + offset);
+        healthSlider.value = currentHealth;
+    }
+    
+}
